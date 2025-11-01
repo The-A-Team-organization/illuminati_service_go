6ac3f66545ebcac44797cb5256325ccedaf59c58
IronMan-1970 <antihetmandb@gmail.com>
2025-11-01 16:14:58 +0200

    TAT-37: added test for postLetter

--- routes/letter_test.go
+++ routes/letter_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	mock_utils "illuminati/go/microservice/utils/mocks"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -49,4 +50,34 @@ func Test_SendLetterEmailWhileRequestBodyIsNil(t *testing.T) {
 
 	err := service.SendLetterEmail(request)
 	assert.Error(t, err)
+}
+
+func Test_PostLetterWhilelsIsNil(t *testing.T) {
+
+	
+	var (
+		ctrl            = gomock.NewController(t)
+		mockEmailSender = mock_utils.NewMockEmailSender(gomock.NewController(t))
+		service         = NewLetterService(mockEmailSender)
+		letter          = Letter{
+			Topic:        "New Letter",
+			Text:         "Here is new letter",
+			TargetEmails: []string{"test1@gmail.com", "test123@gmail.com"},
+		}
+	)
+	defer ctrl.Finish()
+
+	jsonLetter, _ := json.Marshal(letter)
+
+	mockEmailSender.EXPECT().SendEmail(letter.Topic, letter.Text, letter.TargetEmails).Return(nil)
+
+	server := httptest.NewServer(http.HandlerFunc(service.PostLetter))
+	defer server.Close()
+
+	responce, _ := http.Post(server.URL, "application/json" ,bytes.NewBuffer(jsonLetter))
+
+
+
+	assert.Equal(t, responce.Status, "202 Accepted")
+
 }
